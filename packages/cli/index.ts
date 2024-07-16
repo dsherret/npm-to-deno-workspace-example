0f6de77fc18ac1691b74a1096c2530ba6a926361
David Sherret <dsherret@gmail.com>
2024-07-16 10:39:22 -0400

    Add cli

---
+++ packages/cli/index.ts
@@ -0,0 +1,5 @@
+import { add } from "@dsherret/add";
+import { subtract } from "@dsherret/subtract";
+
+console.log(add(1, 2));
+console.log(subtract(4, 2));
---
+++ packages/subtract/index.ts
@@ -0,0 +1,5 @@
+import { add } from "@dsherret/add";
+
+export function subtract(a: number, b: number): number {
+  return add(a, -b);
+}
